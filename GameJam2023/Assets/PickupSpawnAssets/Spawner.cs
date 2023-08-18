4bfbe80825a2b77a1484752c76d7d7c3384cbe19
Adil <bokhariadil@gmail.com>
2023-08-18 16:08:25 +0500

    commit

--- GameJam2023/Assets/PickupSpawnAssets/Spawner.cs
+++ GameJam2023/Assets/PickupSpawnAssets/Spawner.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject[] objectsToSpawn;
-    public float MinWait = 15f;
-    public float MaxWait = 30f;
+    public float MinWait = 6f;
+    public float MaxWait = 10f;
     public float DestoryWait = 5f;
     private void Start()
     {
@@ -20,9 +20,9 @@ public class Spawner : MonoBehaviour
             yield return new WaitForSeconds(interval);
             GameObject objectToSpawn = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
             Vector3 randomPosition = new Vector3(
-                Random.Range(-50f, 50f),
-                3f,
-                Random.Range(-50f, 50f) 
+                Random.Range(-5.8f, 5.8f),
+                1f,
+                Random.Range(-10.18f, 10.18f) 
             );
             GameObject spawnedObject = Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
             Destroy(spawnedObject, DestoryWait);
