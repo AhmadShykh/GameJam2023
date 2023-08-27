c18a577bd7195b4bccf52b3556b4c3871f902377
Ahmad Ali <ahmadshykh2015@gmail.com>
2023-08-27 23:34:09 +0500

    All Done

--- GameJam2023/Assets/Scripts/FinishLevel.cs
+++ GameJam2023/Assets/Scripts/FinishLevel.cs
@@ -12,13 +12,15 @@ public class FinishLevel : MonoBehaviour
     [SerializeField] AudioClip loseClip;
     [SerializeField] AudioSource EventAudioSource;
     [SerializeField] int WorldNum;
+    [SerializeField] int LevelNum;
     public void WinGame()
     {
         EventAudioSource.PlayOneShot(winClip);
         Time.timeScale = 0;
         JoyStick.gameObject.SetActive(false);
         WinCanvas.gameObject.SetActive(true);
-        PlayerPrefs.SetInt("World" + WorldNum.ToString() + "Levels", PlayerPrefs.GetInt("World" + WorldNum.ToString() + "Levels") + 1);
+        if(LevelNum+1 > PlayerPrefs.GetInt("World" + WorldNum.ToString() + "Levels"))
+            PlayerPrefs.SetInt("World" + WorldNum.ToString() + "Levels", LevelNum+1);
     }
 
     public void LoseGame()
--- GameJam2023/Assets/Scripts/PlantResources.cs
+++ GameJam2023/Assets/Scripts/PlantResources.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlantResources : MonoBehaviour
 {
     public int Amount ;
+	[SerializeField] GameObject[] Resource;
     public void DecreaseAmount(int val)
 	{
 		Amount -= val;
+		Destroy(Resource[Amount]);
 		if (Amount <= 0)
+		{
 			Destroy(gameObject);
+		}
+			
 	}
 }
